34fb9a0b0200a89ee1180385ae047e61bf265f0f
gthomas2 <brudinie@gmail.com>
2023-07-21 09:21:48 +0100

    Fix privacy provider namespace. Fix README bad text from search / replace bodge

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-namespace tiny_accessibilitychecker\privacy;
+namespace tiny_customstyleformats\privacy;
 
 /**
  * Privacy Subsystem implementation for the customstyleformats plugin for TinyMCE. *
